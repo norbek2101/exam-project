77b7d4282cbcd58ce65ec5086257aafb28921569
norbek <lightnight1232gmaili.com>
2021-11-10 07:54:26 +0500

    Pagination function added to the project

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,8 @@ from .views import *
 urlpatterns = [
     # Main page
     path('', home, name='home'),
-    path('blog/', blog, name='blog-page'),
+    # path('blog/', blog, name='blog-page'),
+    path('blog/', BlogListView.as_view(), name='blog-page'),
     path('post_detail/<int:pk>/', post_detail, name='post-detail'),
 
     # Authentication
@@ -13,11 +14,22 @@ urlpatterns = [
     path('login/', login, name="signin"),
 
     # Category Pages
-    path('lifestyle/', lifestyle, name="lifestyle"),
-    path('nature/', nature, name="nature"),
-    path('creative/', creative, name="creative"),
-    path('motivation/', motivation, name="motivation"),
-    path('inspiration/', inspiration, name="inspiration"),
-    path('travel/', travel, name="travel"),
+    path('lifestyle/', LifestyleView.as_view(), name='lifestyle'),
+    path('nature/', NatureView.as_view(), name='nature'),
+    path('creative/', CreativeView.as_view(), name='creative'),
+    path('motivation/', MotivationView.as_view(), name='motivation'),
+    path('inspiration/', InspirationView.as_view(), name='inspiration'),
+    path('travel/', TravelView.as_view(), name='travel'),
+
+
+    # path('lifestyle/', lifestyle, name="lifestyle"),
+    # path('nature/', nature, name="nature"),
+    # path('creative/', creative, name="creative"),
+    # path('motivation/', motivation, name="motivation"),
+    # path('inspiration/', inspiration, name="inspiration"),
+    # path('travel/', travel, name="travel"),
+
+
+    path('gotopage/', gotopage, name='gotopage')
  
 ]
--- main/views.py
+++ main/views.py
@@ -1,61 +1,144 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Category, Post
-
+from django.core.paginator import Paginator
+from django.views.generic import  ListView
 # Main page
 def home(request):
     return render(request, 'main/index.html')
 
-def blog(request):
-    return render(request, 'main/pages/blog.html', {'post': Post.objects.all()} )
+
+# def blog(request):
+#     return render(request, 'main/pages/blog.html', {'post': Post.objects.all()})
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'main/pages/blog.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+
 
 def post_detail(request, pk):
-    return render(request, 'main/pages/post-details.html', {'post': Post.objects.get(id=pk)} )
+    return render(request, 'main/pages/post-details.html', {'post': Post.objects.get(id=pk)})
+
 
 # Category  pages
-def lifestyle(request):
-    lifestyle = Category.objects.get(id=1)
-    post = lifestyle.post_set.all()
-    return render(request, 'main/categories/lifestyle.html', {'post': post})
+class LifestyleView(ListView):
+    model = Post
+    template_name = 'main/categories/lifestyle.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        lifestyle = Category.objects.get(id=1)
+        queryset = lifestyle.post_set.all()
+        return  queryset
 
-def nature(request):
-    nature = Category.objects.get(id=2)
-    post = nature.post_set.all()
-    return render(request, 'main/categories/nature.html', {'post': post})
+# def lifestyle(request):
+#     lifestyle = Category.objects.get(id=1)
+#     post = lifestyle.post_set.all()
+#     return render(request, 'main/categories/lifestyle.html', {'post': post})
 
-def creative(request):
-    creative = Category.objects.get(id=3)
-    post = creative.post_set.all()
-    return render(request, 'main/categories/creative.html', {'post': post})
 
-def motivation(request):
-    motivation = Category.objects.get(id=4)
-    post = motivation.post_set.all()
-    return render(request, 'main/categories/motivation.html', {'post': post})
 
-def inspiration(request):
-    inspiration = Category.objects.get(id=5)
-    post = inspiration.post_set.all()
-    return render(request, 'main/categories/inspiration.html', {'post': post})
+class NatureView(ListView):
+    model = Post
+    template_name = 'main/categories/nature.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        nature = Category.objects.get(id=2)
+        queryset = nature.post_set.all()
+        return  queryset
 
-def travel(request):
-    travel = Category.objects.get(id=6)
-    post = travel.post_set.all()
-    return render(request, 'main/categories/travel.html', {'post': post})
+
+# def nature(request):
+#     nature = Category.objects.get(id=2)
+#     post = nature.post_set.all()
+#     return render(request, 'main/categories/nature.html', {'post': post})
+
+
+
+class CreativeView(ListView):
+    model = Post
+    template_name = 'main/categories/creative.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        creative = Category.objects.get(id=3)
+        queryset = creative.post_set.all()
+        return  queryset
+
+
+# def creative(request):
+#     creative = Category.objects.get(id=3)
+#     post = creative.post_set.all()
+#     return render(request, 'main/categories/creative.html', {'post': post})
+
+
+class MotivationView(ListView):
+    model = Post
+    template_name = 'main/categories/motivation.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        motivation = Category.objects.get(id=4)
+        queryset = motivation.post_set.all()
+        return  queryset
+
+# def motivation(request):
+#     motivation = Category.objects.get(id=4)
+#     post = motivation.post_set.all()
+#     return render(request, 'main/categories/motivation.html', {'post': post})
+
+
+class InspirationView(ListView):
+    model = Post
+    template_name = 'main/categories/inspiration.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        inspiration = Category.objects.get(id=5)
+        queryset = inspiration.post_set.all()
+        return  queryset
+
+# def inspiration(request):
+#     inspiration = Category.objects.get(id=5)
+#     post = inspiration.post_set.all()
+#     return render(request, 'main/categories/inspiration.html', {'post': post})
+
+
+
+class TravelView(ListView):
+    model = Post
+    template_name = 'main/categories/travel.html' 
+    context_object_name = 'post'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        travel = Category.objects.get(id=5)
+        queryset = travel.post_set.all()
+        return  queryset
+
+
+# def travel(request):
+#     travel = Category.objects.get(id=6)
+#     post = travel.post_set.all()
+#     return render(request, 'main/categories/travel.html', {'post': post})
+
 
 # Authentication
 def register(request):
     return render(request, 'main/pages/registr.html')
 
+
 def login(request):
     return render(request, 'main/pages/signin.html')
 
 
-
-
-
-
-
-
-
-
-
+def gotopage(request):
+    page = request.GET['page']
+    return redirect('/?page='+page)    
